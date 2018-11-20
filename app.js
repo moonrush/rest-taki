19b96b21ad11195ad4c07f12a6c1eea989adf300
Nora taki <lizhe.0625@foxmail.com>
2018-11-20 14:52:11 +0800

    self ssl for https

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const Koa = require('koa');
 const app = new Koa();
 const bodyParser = require('koa-bodyparser');
 const controller = require('./controller');
+const https = require('https');
+const options = require('./ssl/ssl');
 
 app.use(bodyParser());
 
@@ -11,5 +13,6 @@ app.use(async (ctx, next) => {
 });
 app.use(controller());
 
-app.listen(3000);
+// app.listen(3000);
+https.createServer(options,app.callback()).listen(3000);
 console.log('app started at port 3000...');
